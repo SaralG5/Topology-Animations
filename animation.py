33b98d9cfb41b637fef88c8e3db00516a19415a7
sgau0007 <sgau0007@student.monash.edu>
2023-06-24 19:09:26 +1000

    Finally put edge labels on edges

--- animation.py
+++ animation.py
@@ -1,9 +1,12 @@
+
+
 from manim import *
+
 
 class MyFirstAnimation(Scene):
     def construct(self):
         # Create basic mobjects
-        square = Square()
+        square = Square(side_length=2, color=BLUE)
         label_top = Tex("b")
         label_bottom = Tex("b")
         label_left = Tex("a")
@@ -14,7 +17,14 @@ class MyFirstAnimation(Scene):
         label_left.next_to(square.get_left(), LEFT)
         label_right.next_to(square.get_right(), RIGHT)
 
-        self.add(square,label_top, label_bottom, label_left, label_right)
+        top_midpoint = square.get_edge_center(UP)
+        x_coordinate = (square.get_left()[0] + square.get_right()[0]) / 2
+
+        # make horizontal arrow
+        arrow_horizontal = Arrow([x_coordinate, top_midpoint[1], 0], [x_coordinate, top_midpoint[1], 0], buff=0)
+        arrow_horizontal.next_to(square, UP, buff=0)
+        self.add(square, arrow_horizontal)
+        self.add(square, label_top, label_bottom, label_left, label_right)
 
         # Animate Fade in of the star that takes 2 seconds
         self.play(FadeIn(square, run_time=2))
