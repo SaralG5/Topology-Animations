3553f249c3df1233262f25011331f21381455295
sgau0007 <sgau0007@student.monash.edu>
2023-06-24 16:51:46 +1000

    Finally put edge labels on edges

--- animation.py
+++ animation.py
@@ -4,6 +4,17 @@ class MyFirstAnimation(Scene):
     def construct(self):
         # Create basic mobjects
         square = Square()
+        label_top = Tex("b")
+        label_bottom = Tex("b")
+        label_left = Tex("a")
+        label_right = Tex("a")
+
+        label_top.next_to(square.get_top(), UP)
+        label_bottom.next_to(square.get_bottom(), DOWN)
+        label_left.next_to(square.get_left(), LEFT)
+        label_right.next_to(square.get_right(), RIGHT)
+
+        self.add(square,label_top, label_bottom, label_left, label_right)
 
         # Animate Fade in of the star that takes 2 seconds
         self.play(FadeIn(square, run_time=2))
